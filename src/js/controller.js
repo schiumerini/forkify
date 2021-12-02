3f15884af6ad27fa332c08000ee74da329c427ca
D10S <dschi002@fiu.edu>
2021-12-01 23:01:13 -0500

    Adds update servings feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -55,8 +55,16 @@ const controlPagination = function (goToPage) {
   paginationView.render(model.state.search);
 };
 
+const controlServings = function (newServings) {
+  // 1) Update the recipe servings (in state)
+  model.updateServings(newServings);
+  // 2) Update the recipe view
+  recipeView.render(model.state.recipe);
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  recipeView.addHandlerServings(controlServings);
   searchView.addHandlerRender(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
 };
--- src/js/views/RecipeView.js
+++ src/js/views/RecipeView.js
@@ -38,12 +38,18 @@ class RecipeView extends View {
           <span class="recipe__info-text">servings</span>
 
           <div class="recipe__info-buttons">
-            <button class="btn--tiny btn--increase-servings">
+            <button data-serv="${
+              this._data.servings > 1
+                ? this._data.servings - 1
+                : this._data.servings
+            }" class="btn--tiny btn--update-servings">
               <svg>
                 <use href="${icons}#icon-minus-circle"></use>
               </svg>
             </button>
-            <button class="btn--tiny btn--increase-servings">
+            <button data-serv="${
+              +this._data.servings + 1
+            }" class="btn--tiny btn--update-servings">
               <svg>
                 <use href="${icons}#icon-plus-circle"></use>
               </svg>
@@ -113,6 +119,14 @@ class RecipeView extends View {
 
   addHandlerRender(handler) {
     ['hashchange', 'load'].forEach(ev => window.addEventListener(ev, handler));
+  }
+
+  addHandlerServings(handler) {
+    this._parentElement.addEventListener('click', function (e) {
+      const btn = e.target.closest('.btn--update-servings');
+      if (!btn) return;
+      handler(btn.dataset.serv);
+    });
   }
 }
 
